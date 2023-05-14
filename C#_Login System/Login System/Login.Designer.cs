7616bd9260265bd099be0865ed4fb6db28e3a49f
Sqar <117338622+Sqarp@users.noreply.github.com>
2023-05-14 13:37:54 +0900

    Add files via upload

---
+++ C#_Login System/Login System/Login.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Login_System
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username_box = new System.Windows.Forms.TextBox();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ログイン";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(20, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "パスワード";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(20, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ユーザー名";
+            // 
+            // username_box
+            // 
+            this.username_box.Location = new System.Drawing.Point(22, 31);
+            this.username_box.Name = "username_box";
+            this.username_box.Size = new System.Drawing.Size(215, 19);
+            this.username_box.TabIndex = 9;
+            // 
+            // password_box
+            // 
+            this.password_box.Location = new System.Drawing.Point(21, 82);
+            this.password_box.Name = "password_box";
+            this.password_box.Size = new System.Drawing.Size(215, 19);
+            this.password_box.TabIndex = 10;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 188);
+            this.Controls.Add(this.password_box);
+            this.Controls.Add(this.username_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ログイン";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username_box;
+        private System.Windows.Forms.TextBox password_box;
+    }
+}
+
---
+++ C#_Login System/Login System/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_System
+{
+    public partial class Login : Form
+    {
+        private string username;
+        private string password;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ユーザー名とパスワード
+            username = "Unknown";
+            password = "1234567890";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ユーザー名とパスワードが合っているか判別
+            if ((username_box.Text == username) && (password_box.Text == password))
+            {
+                MessageBox.Show("ログインに成功しました", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            //間違っていたらエラー文を返す
+            else 
+            {
+                MessageBox.Show("ユーザー名かパスワードまたはその両方が間違っています", "失敗", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            //この場合ではどちらが間違っているか分からない一般的なシステムだが,以下のコードでどちらが分かっているか判別することも可
+            //if (username_box.Text == username)
+            //{
+            //    if (password_box.Text == password)
+            //    {
+            //        MessageBox.Show("ログインに成功しました", "成功", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("パスワードが間違っています", "失敗", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("ユーザー名が間違っています", "失敗", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
+    }
+}
